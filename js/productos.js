5f2477b64b58de30d250eae9fbfeec9f700b8574
josecaselli <josejuancaselli@gmail.com>
2024-09-13 17:40:43 -0300

    borre todos los js demas

--- js/productos.js
+++ js/productos.js
@@ -1,25 +1,20 @@
-
 let carritoVacio = JSON.parse(localStorage.getItem("Carro")) || []
-let arrayProductos = []
 const cardsProd = document.getElementById("cards");
+const mostrarCarrito = document.getElementById("mostrar-carrito")
+
 
 function MostrarCarrito(producto) {
     const divProducto = document.createElement("div");
     divProducto.className = "div-producto-carrito";
-    mostrarCarrito.prepend(divProducto);
-
+    
     const titulo = document.createElement("p");
     titulo.innerText = `${producto.nombre} ${producto.color}, ${producto.marca}`;
-    divProducto.appendChild(titulo);
-
+    
     const precioProducto = document.createElement("p");
     precioProducto.innerText = `Precio: $${producto.precio}`;
-    divProducto.appendChild(precioProducto);
     
     const eliminarProducto = document.createElement("button");
     eliminarProducto.innerText = "Eliminar";
-    divProducto.appendChild(eliminarProducto);
-
     eliminarProducto.addEventListener("click", () => {
         const index = carritoVacio.findIndex((objeto) => objeto.id === producto.id);
         if (index >= 0) {
@@ -28,15 +23,20 @@ function MostrarCarrito(producto) {
             divProducto.remove();
         }
     });
+
+    mostrarCarrito.prepend(divProducto);
+    divProducto.appendChild(titulo);
+    divProducto.appendChild(precioProducto);
+    divProducto.appendChild(eliminarProducto);   
 }
 
-
-const crearProductos = async () => {
+const crearProductos = async (categoria) => {
     const respuesta = await fetch("/json/productos.json");
     const datos = await respuesta.json();
     let arrayProductos = datos
-    const musculosas = arrayProductos.filter((el) => el.categoria === "musculosas")
-    musculosas.forEach(item => {
+    const cardProducto = arrayProductos.filter((el) => el.categoria === categoria)
+    console.log(cardProducto)
+    cardProducto.forEach(item => {
         let tarjetas = document.createElement("div")
         tarjetas.innerHTML = `
                     
@@ -72,20 +72,19 @@ const crearProductos = async () => {
                 duration: 3000
                 
                 }).showToast();
+
             localStorage.setItem("Carro", JSON.stringify(carritoVacio));
             MostrarCarrito(producto)
         })
+
         cardsProd.appendChild(tarjetas)
     });
 }
-const mostrarCarrito = document.getElementById("mostrar-carrito")
-
-
-    
 
 carritoVacio.forEach((producto)=>{
     MostrarCarrito(producto);
 })
+
 const btnLinkCarrito = document.createElement("button");
 btnLinkCarrito.innerText = "Ir al carrito";
 mostrarCarrito.appendChild(btnLinkCarrito);
@@ -93,8 +92,26 @@ btnLinkCarrito.addEventListener("click", () => {
     window.location.href  = "/pages/carrito.html";
 })
 
-document.addEventListener("DOMContentLoaded", crearProductos());
-
-
-
-
+document.addEventListener("DOMContentLoaded", () => {
+    let categoria = "";
+    
+    if (window.location.pathname.includes("remeras")) {
+        categoria = "remeras";
+    } else if (window.location.pathname.includes("pantalon")) {
+        categoria = "pantalon";
+    }else if (window.location.pathname.includes("camisas")) {
+        categoria = "camisas";
+    }else if (window.location.pathname.includes("zapatillas")) {
+        categoria = "zapatillas";
+    }else if (window.location.pathname.includes("musculosas")) {
+        categoria = "musculosas";
+    }else if (window.location.pathname.includes("polleras")) {
+        categoria = "polleras";
+    }else if (window.location.pathname.includes("vestidos")) {
+        categoria = "vestidos";
+    }else if (window.location.pathname.includes("zapa_mujer")) {
+        categoria = "zapa_mujer";
+    }
+    
+    crearProductos(categoria);
+});
