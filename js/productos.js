6ddc64b5d86567716bb0dcb5891decc5f4a57e29
josecaselli <josejuancaselli@gmail.com>
2024-09-14 01:59:11 -0300

    unificacion js

--- js/productos.js
+++ js/productos.js
@@ -2,40 +2,31 @@ let carritoVacio = JSON.parse(localStorage.getItem("Carro")) || []
 const cardsProd = document.getElementById("cards");
 const mostrarCarrito = document.getElementById("mostrar-carrito")
 
+// document.addEventListener("DOMContentLoaded", () => {
+//     fetch('/json/productos.json')  //me traigo el json
+//         .then(response => response.json())
+//         .then(productos => {
+            
+//             const categorias = productos.map((p) => p.categoria); //me traigo las categorias y las guardo como un nuevo array en una variable
+//             const generos = productos.map((p)=> p.genero)
 
-function MostrarCarrito(producto) {
-    const divProducto = document.createElement("div");
-    divProducto.className = "div-producto-carrito";
-    
-    const titulo = document.createElement("p");
-    titulo.innerText = `${producto.nombre} ${producto.color}, ${producto.marca}`;
-    
-    const precioProducto = document.createElement("p");
-    precioProducto.innerText = `Precio: $${producto.precio}`;
-    
-    const eliminarProducto = document.createElement("button");
-    eliminarProducto.innerText = "Eliminar";
-    eliminarProducto.addEventListener("click", () => {
-        const index = carritoVacio.findIndex((objeto) => objeto.id === producto.id);
-        if (index >= 0) {
-            carritoVacio.splice(index, 1);
-            localStorage.setItem("Carro", JSON.stringify(carritoVacio));
-            divProducto.remove();
-        }
-    });
+//             const categoriaActual = categorias.find((categoria) => window.location.pathname.includes(categoria)) || ""; //recorro el array de categorias y cuando encuentra una que incluya a la categoria la guardo en la variable
+//             const generoActual = generos.find((genero) => window.location.pathname.includes(genero)) || "";
 
-    mostrarCarrito.prepend(divProducto);
-    divProducto.appendChild(titulo);
-    divProducto.appendChild(precioProducto);
-    divProducto.appendChild(eliminarProducto);   
-}
 
-const crearProductos = async (categoria) => {
+//             crearProductos(categoriaActual || generoActual || ""); //meto la categoria actual en la funcion crearProductos, y esto desata toda la funcion
+//         })
+//         .catch(error => {
+//             console.error('There was a problem with the fetch operation:', error);
+//         });
+// });
+
+const crearProductos = async (arrayRopa) => { // aca entra la categoria que me traje
     const respuesta = await fetch("/json/productos.json");
     const datos = await respuesta.json();
     let arrayProductos = datos
-    const cardProducto = arrayProductos.filter((el) => el.categoria === categoria || el.genero === categoria)
-    
+    const cardProducto = arrayProductos.filter((el) => el.categoria === arrayRopa || el.genero === arrayRopa)
+
     cardProducto.forEach(item => {
         let tarjetas = document.createElement("div")
         tarjetas.innerHTML = `
@@ -64,58 +55,97 @@ const crearProductos = async (categoria) => {
                 marca: producto.marca,
                 color: producto.color,
                 precio: producto.precio,
+                categoria: producto.categoria,
+
             })
             Toastify({
 
                 text: `Se agregó ${producto.nombre} ${producto.color} ${producto.marca}`,
-                
+
                 duration: 3000
-                
-                }).showToast();
+
+            }).showToast();
 
             localStorage.setItem("Carro", JSON.stringify(carritoVacio));
-            //MostrarCarrito(producto)
+            MostrarCarrito(producto)
         })
 
         cardsProd.appendChild(tarjetas)
     });
+
+
+    
 }
 
-carritoVacio.forEach((producto)=>{
+function MostrarCarrito(producto) {
+    const divProducto = document.createElement("div");
+    divProducto.className = "div-producto-carrito";
+
+    const titulo = document.createElement("p");
+    titulo.innerText = `${producto.nombre} ${producto.color}, ${producto.marca}`;
+
+    const precioProducto = document.createElement("p");
+    precioProducto.innerText = `Precio: $${producto.precio}`;
+
+    const eliminarProducto = document.createElement("button");
+    eliminarProducto.innerText = "Eliminar";
+    eliminarProducto.addEventListener("click", () => {
+        const index = carritoVacio.findIndex((objeto) => objeto.id === producto.id);
+        if (index >= 0) {
+            carritoVacio.splice(index, 1);
+            localStorage.setItem("Carro", JSON.stringify(carritoVacio));
+            divProducto.remove();
+        }
+    });
+
+    mostrarCarrito.prepend(divProducto);
+    divProducto.appendChild(titulo);
+    divProducto.appendChild(precioProducto);
+    divProducto.appendChild(eliminarProducto);
+}
+
+
+
+
+carritoVacio.forEach((producto) => {
     MostrarCarrito(producto);
 })
-
+let arrayProductos = []
 const btnLinkCarrito = document.createElement("button");
 btnLinkCarrito.innerText = "Ir al carrito";
 mostrarCarrito.appendChild(btnLinkCarrito);
 btnLinkCarrito.addEventListener("click", () => {
-    window.location.href  = "/pages/carrito.html";
+    window.location.href = "/pages/carrito.html";
 })
+
+
+
+
 
 document.addEventListener("DOMContentLoaded", () => {
     let categoria = "";
-    
+
     if (window.location.pathname.includes("remeras")) {
         categoria = "remeras";
     } else if (window.location.pathname.includes("pantalon")) {
         categoria = "pantalon";
-    }else if (window.location.pathname.includes("camisas")) {
+    } else if (window.location.pathname.includes("camisas")) {
         categoria = "camisas";
-    }else if (window.location.pathname.includes("zapatillas")) {
+    } else if (window.location.pathname.includes("zapatillas")) {
         categoria = "zapatillas";
-    }else if (window.location.pathname.includes("musculosas")) {
+    } else if (window.location.pathname.includes("musculosas")) {
         categoria = "musculosas";
-    }else if (window.location.pathname.includes("polleras")) {
+    } else if (window.location.pathname.includes("polleras")) {
         categoria = "polleras";
-    }else if (window.location.pathname.includes("vestidos")) {
+    } else if (window.location.pathname.includes("vestidos")) {
         categoria = "vestidos";
-    }else if (window.location.pathname.includes("zapa_mujer")) {
+    } else if (window.location.pathname.includes("zapa_mujer")) {
         categoria = "zapa_mujer";
-    }else if (window.location.pathname.includes("ropa_femenina")) {
+    } else if (window.location.pathname.includes("mujer")) {
         categoria = "mujer";
-    }else if (window.location.pathname.includes("ropa_masculina")) {
+    } else if (window.location.pathname.includes("hombre")) {
         categoria = "hombre";
     }
-    
+
     crearProductos(categoria);
 });
